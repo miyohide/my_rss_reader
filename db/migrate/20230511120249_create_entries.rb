f9b7b20420d66fa5be358a7c95b9ddbd567c11ca
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-11 21:06:09 +0900

    add column modifier

--- db/migrate/20230511120249_create_entries.rb
+++ db/migrate/20230511120249_create_entries.rb
@@ -2,10 +2,10 @@ class CreateEntries < ActiveRecord::Migration[7.0]
   def change
     create_table :entries do |t|
       t.references :feed, null: false, foreign_key: true
-      t.string :title
-      t.string :link
-      t.text :body
-      t.datetime :published_at
+      t.string :title, null: false
+      t.string :link, null: false
+      t.text :body, null: false
+      t.datetime :published_at, null: false
 
       t.timestamps
     end
