2d2e9f4d99718f183e4cff2d8c21cae909741378
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-11 21:01:10 +0900

    add column modifier

--- db/migrate/20230511115713_create_feeds.rb
+++ db/migrate/20230511115713_create_feeds.rb
@@ -1,8 +1,8 @@
 class CreateFeeds < ActiveRecord::Migration[7.0]
   def change
     create_table :feeds do |t|
-      t.string :title
-      t.string :endpoint
+      t.string :title, null: false, limit: 100
+      t.string :endpoint, null: false, limit: 250
 
       t.timestamps
     end
