fc1135edc058acd8402a01f1d02ac336ab42d15e
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-12 21:13:33 +0900

    implement parsed_xml

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,11 @@
+require 'net/http'
+require 'rss'
+
 class Feed < ApplicationRecord
   has_many :entries
+
+  def parsed_xml
+    xml = Net::HTTP.get(URI.parse(endpoint))
+    RSS::Parser.parse(xml)
+  end
 end
