694a617ac5ffb5b4b753bd6ea4dd4f04339775d8
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-13 20:39:59 +0900

    implement execute method that save entry data.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -8,4 +8,20 @@ class Feed < ApplicationRecord
     xml = Net::HTTP.get(URI.parse(endpoint))
     RSS::Parser.parse(xml)
   end
+
+  def execute
+    parsed_xml.items.map do |item|
+      Entry.create(
+        feed: self,
+        title: item.title,
+        body: strip_tags(item.description).truncate(200),
+        published_at: item.pubDate,
+        link: item.link
+      )
+    end
+  end
+
+  def strip_tags(text)
+    ActionController::Base.helpers.strip_tags(text)
+  end
 end
