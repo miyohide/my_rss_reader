f4fd2a065fb77c7725bcd787656bae405372c1d8
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-14 18:26:13 +0900

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_120249) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_091949) do
   create_table "entries", force: :cascade do |t|
     t.integer "feed_id", null: false
     t.string "title", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_120249) do
     t.string "endpoint", limit: 250, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "last_updated_at"
   end
 
   create_table "posts", force: :cascade do |t|
