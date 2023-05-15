503fcfc4dbcecb3e987b1e56349342e69e0fb273
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-15 20:45:07 +0900

    set root path to welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   resources :entries
   resources :feeds
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'posts#index'
+  root 'welcome#index'
 end
