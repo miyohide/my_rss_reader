4a90cda2d177c3d32eb7a23e68ae3486ee33fa50
Hidenori Miyoshi <miyohide@gmail.com>
2023-05-24 21:07:41 +0900

    display latest 10 entries

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,7 +3,7 @@ class FeedsController < ApplicationController
 
   # GET /feeds or /feeds.json
   def index
-    @feeds = Feed.all
+    @feeds = Feed.includes(:entries).all
   end
 
   # GET /feeds/1 or /feeds/1.json
