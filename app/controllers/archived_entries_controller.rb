4edc95745817cd0db05ebcf2189712e25430ee94
Hidenori Miyoshi <miyohide@gmail.com>
2023-06-21 20:17:57 +0900

    order by published_at

--- app/controllers/archived_entries_controller.rb
+++ app/controllers/archived_entries_controller.rb
@@ -3,7 +3,7 @@ class ArchivedEntriesController < ApplicationController
 
   # GET /archived_entries or /archived_entries.json
   def index
-    @archived_entries = ArchivedEntry.all.page(params[:page])
+    @archived_entries = ArchivedEntry.order(published_at: :desc).page(params[:page])
   end
 
   # GET /archived_entries/1 or /archived_entries/1.json
